1926df77fdfd2098eaf5391e8533f4850ce47bc7
dvvinfo <dvvinfo@mail.ru>
2023-01-14 21:10:21 +0300

    адаптировал страницы

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,49 +1,48 @@
 const sidebarFixed = document.querySelector(".sidebar-fixed");
 const header = document.querySelector(".header-fixed ");
-const avatarWrapper = document.querySelector('.avatar-wrapper')
-const iconsBlockOne = document.querySelector('#icons-block-one') 
-const iconsBlockTwo = document.querySelector('#icons-block-two ') 
-const avatarIcon = document.querySelector(".avatar-icon")
-const profile = document.querySelector("#profile ") 
-const sidebar = document.querySelector(".w-2") 
-const btnClose = document.querySelector(".btn-close")
-const searchIcon = document.querySelector('#search-icon ')
-const searchForm = document.querySelector('.search-form')
-const burger = document.querySelector('.burger')
-const rightBlock = document.querySelector('.right-block')
-const categoryHidden = document.querySelector('.category-hidden')
-const categoryBtnClose = document.querySelector('.category-btn-close')
-const categoryOpen = document.querySelector('#category-open')
+const avatarWrapper = document.querySelector(".avatar-wrapper");
+const iconsBlockOne = document.querySelector("#icons-block-one");
+const iconsBlockTwo = document.querySelector("#icons-block-two ");
+const avatarIcon = document.querySelector(".avatar-icon");
+const profile = document.querySelector("#profile ");
+const sidebar = document.querySelector(".w-2");
+const btnClose = document.querySelector(".btn-close");
+const searchIcon = document.querySelector("#search-icon ");
+const searchForm = document.querySelector(".search-form");
+const burger = document.querySelector(".burger");
+const rightBlock = document.querySelector(".right-block");
+const categoryHidden = document.querySelector(".category-hidden");
+const categoryBtnClose = document.querySelector(".category-btn-close");
+const categoryOpen = document.querySelector("#category-open");
+const navMobile = document.querySelector(".nav-mobile");
 
-if (sidebarFixed  ) {
+if (sidebarFixed) {
   window.addEventListener("scroll", scroll);
 }
-if (header ) {
-    window.addEventListener('scroll', scrollHeader)
+if (header) {
+  window.addEventListener("scroll", scrollHeader);
 }
 
 function scroll() {
   if (window.scrollY > 100) {
     sidebarFixed.classList.add("fixed");
-    console.log("kkk");
   } else {
     sidebarFixed.classList.remove("fixed");
   }
 }
 function scrollHeader() {
-    if (window.scrollY > 100) {
-        header.classList.add('sticky')
-    } else {
-        header.classList.remove('sticky')
-    }
+  if (window.scrollY > 100) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
 if (avatarWrapper) {
-  avatarWrapper.addEventListener('click', () => {
-    iconsBlockOne.classList.toggle('translate-show')
-    iconsBlockTwo.classList.toggle('translate-show')
-    avatarIcon.classList.toggle('transform-arrow')
-    sidebar.classList.toggle('sidebar-show')
-  })
+  avatarWrapper.addEventListener("click", () => {
+    navMobile.classList.toggle("nav-mobile-show");
+    avatarIcon.classList.toggle("transform-arrow");
+    sidebar.classList.toggle("sidebar-show");
+  });
 }
 // if (profile) {
 //   profile.addEventListener('click', () => {
@@ -59,32 +58,43 @@ if (avatarWrapper) {
 // }
 
 if (searchIcon) {
-  searchIcon.addEventListener('click', () => {
-    searchIcon.classList.toggle('icon-color')
-    searchForm.classList.toggle('search-show')
-  })
+  searchIcon.addEventListener("click", () => {
+    searchIcon.classList.toggle("icon-color");
+    searchForm.classList.toggle("search-show");
+  });
 }
 
 if (burger) {
-  burger.addEventListener('click', () => {
-    rightBlock.classList.toggle('right-block-show')
-  })
+  burger.addEventListener("click", () => {
+    rightBlock.classList.toggle("right-block-show");
+  });
 }
 
 if (categoryOpen) {
-  categoryOpen.addEventListener('click', () => {
-    categoryHidden.classList.add('category-open')
-  })
+  categoryOpen.addEventListener("click", () => {
+    categoryHidden.classList.add("category-open");
+  });
 }
 
 if (categoryBtnClose) {
   categoryBtnClose.addEventListener("click", () => {
-    categoryHidden.classList.remove('category-open')
-  })
+    categoryHidden.classList.remove("category-open");
+  });
 }
 
+document.addEventListener("click", (e) => {
+  if (
+    !e.target.closest(".nav-mobile") &&
+    !e.target.closest(".avatar-wrapper")
+  ) {
+    navMobile.classList.remove("nav-mobile-show");
+  }
+});
+
 // document.addEventListener('click', (e) => {
-//   if (!e.target.closest('.category-hidden') && !e.target.closest('.category-btn-close')) {
-//     categoryHidden.classList.remove('category-open')
+//   const click = e.composedPath().includes(navMobile)
+//   console.log(click)
+//   if (!click) {
+//     navMobile.style.display='none'
 //   }
 // })
