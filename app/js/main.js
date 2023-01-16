01a93744f3387ce5c662d5596bfdec3c94d14499
dvvinfo <dvvinfo@mail.ru>
2023-01-16 17:31:25 +0300

    пофиксил

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,6 @@
 const sidebarFixed = document.querySelector(".sidebar-fixed");
 const header = document.querySelector(".header-fixed ");
 const avatarWrapper = document.querySelector(".avatar-wrapper");
-const iconsBlockOne = document.querySelector("#icons-block-one");
-const iconsBlockTwo = document.querySelector("#icons-block-two ");
-const avatarIcon = document.querySelector(".avatar-icon");
 const profile = document.querySelector("#profile ");
 const sidebar = document.querySelector(".hidden");
 const btnClose = document.querySelector(".btn-close");
@@ -15,9 +12,13 @@ const categoryHidden = document.querySelector(".category-hidden");
 const categoryBtnClose = document.querySelector(".category-btn-close");
 const categoryOpen = document.querySelector("#category-open");
 const navMobile = document.querySelector(".nav-mobile");
-const navBtnClose = document.querySelector('.nav-mobile__btn-close')
-const btnCloseRightBlock = document.querySelector(".btn-close-right-block")
-const hiddenBlock = document.querySelector(".hidden-block")
+const navBtnClose = document.querySelector(".nav-mobile__btn-close");
+const btnCloseRightBlock = document.querySelector(".btn-close-right-block");
+const hiddenBlock = document.querySelector(".hidden-block");
+const postsBtnClose = document.querySelector(".posts-btn-close");
+const profileHidden = document.querySelector(".profile-hidden");
+const burgerPersonBtn = document.querySelector(".burger-person ");
+const btnCloseProfile = document.querySelector(".btn-close-profile");
 
 if (sidebarFixed) {
   window.addEventListener("scroll", scroll);
@@ -43,17 +44,32 @@ function scrollHeader() {
 if (avatarWrapper) {
   avatarWrapper.addEventListener("click", () => {
     navMobile.classList.toggle("nav-mobile-show");
-     avatarIcon.classList.toggle("transform-arrow");
     if (sidebar) {
       sidebar.classList.toggle("sidebar-show");
     }
-    
+  });
+}
+
+if (burgerPersonBtn) {
+  burgerPersonBtn.addEventListener("click", () => {
+    profileHidden.classList.add("profile-show");
+  });
+}
+if (btnCloseProfile) {
+  btnCloseProfile.addEventListener("click", () => {
+    profileHidden.classList.remove("profile-show");
+  });
+}
+
+if (postsBtnClose) {
+  postsBtnClose.addEventListener("click", () => {
+    sidebar.classList.remove("sidebar-show");
   });
 }
 if (navBtnClose) {
-  navBtnClose.addEventListener('click', () => {
+  navBtnClose.addEventListener("click", () => {
     navMobile.classList.remove("nav-mobile-show");
-  })  
+  });
 }
 
 if (searchIcon) {
@@ -68,23 +84,22 @@ if (burger) {
     if (rightBlock) {
       rightBlock.classList.add("right-block-show");
     }
-    
+
     if (hiddenBlock) {
-      hiddenBlock.classList.add('right-block-show')
+      hiddenBlock.classList.add("right-block-show");
     }
   });
 }
 if (btnCloseRightBlock) {
-  btnCloseRightBlock.addEventListener("click",() => {
+  btnCloseRightBlock.addEventListener("click", () => {
     if (rightBlock) {
       rightBlock.classList.remove("right-block-show");
     }
-    
+
     if (hiddenBlock) {
-      hiddenBlock.classList.remove('right-block-show')
+      hiddenBlock.classList.remove("right-block-show");
     }
-    
-  })
+  });
 }
 
 if (categoryOpen) {
@@ -110,14 +125,10 @@ document.addEventListener("click", (e) => {
   }
 });
 document.addEventListener("click", (e) => {
-  if (
-    !e.target.closest(".hidden-block") &&
-    !e.target.closest(".burger")
-  ) {
-    if(hiddenBlock){
+  if (!e.target.closest(".hidden-block") && !e.target.closest(".burger")) {
+    if (hiddenBlock) {
       hiddenBlock.classList.remove("right-block-show");
     }
-    
   }
 });
 
@@ -126,17 +137,18 @@ document.addEventListener("click", (e) => {
     !e.target.closest(".category-hidden") &&
     !e.target.closest(".category__block")
   ) {
-    if(categoryHidden){
+    if (categoryHidden) {
       categoryHidden.classList.remove("category-open");
     }
-    
   }
 });
-
-// document.addEventListener('click', (e) => {
-//   const click = e.composedPath().includes(navMobile)
-//   console.log(click)
-//   if (!click) {
-//     navMobile.style.display='none'
-//   }
-// })
+document.addEventListener("click", (e) => {
+  if (
+    !e.target.closest(".profile-hidden") &&
+    !e.target.closest(".burger-person")
+  ) {
+    if (categoryHidden) {
+      profileHidden.classList.remove("profile-show");
+    }
+  }
+});
